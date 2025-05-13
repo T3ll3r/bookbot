91e75fbdbcae14d8a1a4caa1ca48ff5f2e33dfc5
T3ll3r <tony.r.frampton30@gmail.com>
2025-05-13 08:31:21 -0400

    added get_book_character_count_by_letter function

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from stats import get_book_text_count
+from stats import *
 
 def main():
     book_path = 'books/frankenstein.txt'
     #text = get_book_text(book_path)
-    text_count = get_book_text_count(book_path)
-    print(f"{text_count} words found in the document")
-
-def get_book_text(file_path):
-    # This function reads the content of a book file and returns it as a string.
-    with open(file_path) as f:
-        book_text = f.read()
-    print(book_text) 
-
-
-
+    #text_count = get_book_text_count(book_path)
+    #print(f"{text_count} words found in the document")
+    char_count = get_book_character_count_by_letter(book_path)
+    print(char_count)
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,27 @@
+
+def get_book_text(file_path):
+    # This function reads the content of a book file and returns it as a string.
+    with open(file_path) as f:
+        book_text = f.read()
+    print(book_text) 
+
+
 def get_book_text_count(file_path):
     with open(file_path) as f:
         book_text = f.read()
     word_count = len(book_text.split())
     return word_count
-    
+
+
+def get_book_character_count_by_letter(file_path):
+    with open(file_path) as f:
+        book_text = f.read()
+        lowered_book_text = book_text.lower()
+    # Count the number of characters in the book text
+    char_count = len(lowered_book_text)
+    # Count the number of characters by letter
+    char_count_by_letter = {}
+    for char in lowered_book_text:
+       # if char.isalpha():
+            char_count_by_letter[char] = char_count_by_letter.get(char, 0) + 1
+    return char_count_by_letter
