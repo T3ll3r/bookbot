d785da32e26a5078cb6bd0716c152451ec291a75
John Doe <john.doe@contoso.com>
2025-05-14 07:33:59 -0400

    adding mobydick.txt and prideandprejudice.txt

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
-from stats import *
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+from sys import argv
+
+if len(argv) != 2:
+    exit(1)
+else:
+    book_path = argv[1]
+
 
 def main():
-    book_path = 'books/frankenstein.txt'
-    #text = get_book_text(book_path)
-    #text_count = get_book_text_count(book_path)
-    #print(f"{text_count} words found in the document")
-    char_count = get_book_character_count_by_letter(book_path)
-    print(char_count)
+
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
 main()
--- stats.py
+++ stats.py
@@ -1,27 +1,26 @@
-
-def get_book_text(file_path):
-    # This function reads the content of a book file and returns it as a string.
-    with open(file_path) as f:
-        book_text = f.read()
-    print(book_text) 
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
-def get_book_text_count(file_path):
-    with open(file_path) as f:
-        book_text = f.read()
-    word_count = len(book_text.split())
-    return word_count
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
-def get_book_character_count_by_letter(file_path):
-    with open(file_path) as f:
-        book_text = f.read()
-        lowered_book_text = book_text.lower()
-    # Count the number of characters in the book text
-    char_count = len(lowered_book_text)
-    # Count the number of characters by letter
-    char_count_by_letter = {}
-    for char in lowered_book_text:
-       # if char.isalpha():
-            char_count_by_letter[char] = char_count_by_letter.get(char, 0) + 1
-    return char_count_by_letter
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
